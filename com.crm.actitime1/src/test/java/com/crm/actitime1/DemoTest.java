502ca7e068a16b455a10e5b9560d1c0fb5873bdf
shrey <shrey@Shreya>
2023-01-06 16:54:26 +0530

    Commit1

---
+++ com.crm.actitime1/src/test/java/com/crm/actitime1/DemoTest.java
@@ -0,0 +1,11 @@
+package com.crm.actitime1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+@Test
+public void demoTest() {
+	Reporter.log("hii",true);
+}
+}
